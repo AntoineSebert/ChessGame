6db1782f84609b1b8f2a19701444880d027505be
Antoine <antoine.sb@orange.fr>
2017-11-07 16:41:08 +0000

    Reborn #2

---
+++ Chessgame/Chessgame/Singleton.h
@@ -0,0 +1,15 @@
+/*
+	@author Antoine "Anthony" Sébert
+	@creation_date 04/11/2017
+	@description thread-safe templated singleton class using magic statics
+*/
+
+#pragma once
+
+template<typename T> class Singleton {
+	public:
+	static T& getInstance() {
+		static T instance;
+		return instance;
+	}
+};
--- Chessgame/Chessgame/header.h
+++ Chessgame/Chessgame/header.h
@@ -1,6 +1,9 @@
-#pragma once
 /*
 	@author Antoine "Anthony" Sébert
 	@creation_date 07/11/2017
 	@description header file for main
 */
+
+#pragma once
+
+#include "Game.h"
