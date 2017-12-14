28f3816f164765c016abfe6b41b1ea1c69cf88ad
Antoine <antoine.sb@orange.fr>
2017-12-14 17:03:28 +0000

    Commentaires dans la classe Game

--- Chessgame/Game.cpp
+++ Chessgame/Game.cpp
@@ -14,11 +14,11 @@ using json = nlohmann::json;
 		gameInterface = &Interface::getInstance();
 		gameMode = setGameModes();
 		initializePlayers();
-		//difficulty = setDifficulty();
-		//gameBoard = &gameBoard->getInstance();
-		//initializeBoard();
-		//initializeArmies();
-		//gameLoop();
+		difficulty = setDifficulty();
+		gameBoard = &gameBoard->getInstance();
+		initializeBoard();
+		initializeArmies();
+		gameLoop();
 	}
 	difficulties Game::getDifficulty() { return difficulty; }
 	gameModes Game::getGameMode() { return gameMode; }
@@ -30,14 +30,16 @@ using json = nlohmann::json;
 		while (turns < 2 || isGameFinished()) {
 			++turns;
 			gameBoard->display(turns);
+			// select next player to play, make him play, record move
 			gameProgress.push_back(selectPlayerToPlay(turns).lock()->play(gameBoard));
 		}
 	}
 	bool Game::isGameFinished() {
-		// tester les deux rois, si les pièces peuvent bouger, et d'autres trucs
+		// end of game tests
 		return false;
 	}
-	std::weak_ptr<Player> Game::selectPlayerToPlay(unsigned int turns) {
+	weak_ptr<Player> Game::selectPlayerToPlay(unsigned int turns) {
+		// works for a number N of players
 		return players.at(turns % players.size());
 	}
 	void Game::reinitializeCallbacks() {
@@ -49,31 +51,41 @@ using json = nlohmann::json;
 	// import / export
 		void Game::exportToFile() {
 			json container = convertToJson();
+			// creation of the output stream
 			ofstream o("output.json");
-			o << std::setw(4) << container << endl;
+			// formatting and writing
+			o << setw(4) << container << endl;
 		}
 		json Game::convertToJson() {
 			json container;
+			// date time
 			container["date"] = currentDatetimeToString();
+			// players
 			container["players"] = json::array();
 			for (shared_ptr<Player> player : players)
 				container["players"].push_back(player->getName());
+			// first player to play
 			container["firstPlayerToPlay"] = firstToPlay.lock()->getName();
+			// moves
 			container["progress"] = json::array();
 			for (tuple<boardCoord, boardCoord> move : gameProgress)
 				container["progress"].push_back(move);
 			return container;
 		}
 		bool Game::importFile() {
-			std::ifstream i("input.json");
+			// creation of the input stream
+			ifstream i("input.json");
 			json container;
+			// writing data into container
 			i >> container;
+			// check if input is correct
 			if (!checkJSONValidity(container))
 				return false;
-			list<std::tuple<boardCoord, boardCoord>> temp;
+			// do not write in object member 'gameProgress' yet, but in temporary variable
+			list<tuple<boardCoord, boardCoord>> temp;
 			for (unsigned int i = 0; i < players.size(); ++i)
 				players.at(i) = make_shared<Player>(container["players"].at(i), (container["players"].at(i) == container["firstPlayerToPlay"] ? 0 : 1), gameInterface);
-			gameProgress.clear();
+			// copying moves
 			for (json move : container["progress"]) {
 				if (move.is_array())
 					temp.push_back(
@@ -85,25 +97,36 @@ using json = nlohmann::json;
 				else
 					return false;
 			}
+			// clear the member
+			gameProgress.clear();
+			// copying the data
 			copy(temp.begin(), temp.end(), gameProgress.begin());
 			return true;
 		}
-		std::string Game::currentDatetimeToString() {
+		string Game::currentDatetimeToString() {
+			// time container
 			time_t rawtime;
+			// put  urrent time into container
 			time(&rawtime);
+			// time information container
 			struct tm timeinfo;
+			// put human-readable datetime in time information container
 			localtime_s(&timeinfo, &rawtime);
+			// creating the buffer with sufficient space
 			char buffer[80];
+			// convert time to a sequence of characters
 			strftime(buffer, 80, "%c", &timeinfo);
 			return buffer;
 		}
 		bool Game::checkJSONValidity(json object) {
+			// check of required objects exists
 			if (
 				object.find("date") != object.end() &&
 				object.find("players") != object.end() &&
 				object.find("firstPlayerToPlay") != object.end() &&
 				object.find("progress") != object.end()
 				) {
+				// check the types & contents
 				if (
 					object["players"].is_array() && object["players"].size() == 2 &&
 					object["date"].is_string() && object["date"].is_null() &&
@@ -129,11 +152,16 @@ using json = nlohmann::json;
 			return EVE;
 		}
 		void Game::initializePlayers() {
+			// creating an array for the names
 			array<string, 2> names;
+			// fill array with players names
 			initializePlayersNames(&names);
+			// select the player to play first from the names array
 			unsigned int firstToPlayIndex = setWhoPlaysFirst(&names);
+			// creating the players objects in the member 'players'
 			for (unsigned int i = 0; i < players.size(); ++i)
 				players[i] = make_shared<Player>(names[i], (i == firstToPlayIndex ? 0 : 1), gameInterface);
+			// setting the pointer to the player to play first
 			firstToPlay = players[firstToPlayIndex];
 		}
 		void Game::initializePlayersNames(array<string, 2>* names) {
@@ -153,11 +181,12 @@ using json = nlohmann::json;
 			}
 		}
 		void Game::chooseHumanPlayerName(string* target, vector<string>* alreadyTakenNames) {
+			// while the chosen name already exists, choose an other name
 			do {
 				*target = gameInterface->alphanumericalChoice(
 					&(vector<string>() = {
-					"Type the name of the human player (names must be different)"
-				})
+						"Type the name of the human player (names must be different)"
+					})
 				);
 			} while (find(alreadyTakenNames->begin(), alreadyTakenNames->end(), *target) != alreadyTakenNames->end());
 		}
@@ -170,6 +199,7 @@ using json = nlohmann::json;
 			return gameInterface->numberChoice(&labels, (unsigned int)labels.size() - 1);
 		}
 		difficulties Game::setDifficulty() {
+			// if two humans are playing, difficulty is not needed
 			if (gameMode != PVP) {
 				vector<string> labels = {
 					"Select the difficulty",
